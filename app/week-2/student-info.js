c04f34b87cf07bd7a778b6412e4a525d53c1c617
Jashangill1 <jashanpreet.singh03@edu.sait.ca>
2024-05-27 16:20:51 -0600

    first commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main className="h-screen">
+        <p>Jashanpreet Singh</p>
+        <Link href="https://github.com/Jashangill1">My GitHub account</Link>
+      </main>
+    );
+  }
