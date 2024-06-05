5e3d5c3fa9b1057d17aed93b116cfec4439df983
Jashangill1 <jashanpreet.singh03@edu.sait.ca>
2024-06-05 10:35:56 -0600

    Intial Commit

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen">
+    <main className="h-screen flex flex-col">
       <h1 className="text-xl">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="./week-2/">Week 2 Assignment</Link>
+      <Link href="./week-3/">Week 3 Assignment</Link>
     </main>
   );
 }
