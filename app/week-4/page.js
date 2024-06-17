666684cafc68ef9a7858fcbc717f54d085826478
Jashangill1 <jashanpreet.singh03@edu.sait.ca>
2024-06-17 14:42:52 -0600

    Added week 4 content

---
+++ app/week-4/page.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import NewItem from './new-item'
+
+function page() {
+  return (
+    <main className='justify-center items-center mt-50'>
+        <h2 className='flex justify-center text-xl font-bold text-gray-50'></h2>
+        <NewItem />
+    </main>
+  )
+}
+
+export default page
